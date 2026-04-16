dd7018febac489fb7f30aef6890ffa77eee5dde8
Janybanny <codeberg@janybanny.moe>
2026-04-16 16:18:00 +0200

    WIP Forms 1

--- SWEN2TourPlanner.Frontend/ViewModels/CurrentTourViewModel.cs
+++ SWEN2TourPlanner.Frontend/ViewModels/CurrentTourViewModel.cs
@@ -3,5 +3,7 @@ using SWEN2TourPlanner.Frontend.ViewModels.Interfaces;
 namespace SWEN2TourPlanner.Frontend.ViewModels;
 
 public class CurrentTourViewModel : ICurrentTourViewModel {
-    
+    public bool EditMode { get; set; }
+    public string Distance { get; set; } = "3km";
+    public string Time { get; set; } = "3h20";
 }
--- SWEN2TourPlanner.Frontend/ViewModels/Interfaces/ICurrentTourViewModel.cs
+++ SWEN2TourPlanner.Frontend/ViewModels/Interfaces/ICurrentTourViewModel.cs
@@ -1,5 +1,7 @@
 namespace SWEN2TourPlanner.Frontend.ViewModels.Interfaces;
 
 public interface ICurrentTourViewModel {
-    
+    bool EditMode { get; set; }
+    String Distance { get; set; }
+    String Time { get; set; }
 }
